4044dfc76f7db587e01cc05788ea7b96fc7944f5
Mubin <mubin@kolpolok.com>
2025-08-07 02:10:12 +0600

    changes: domain module repo interface created

---
+++ domain/src/main/java/com/mubin/domain/repo/MovieRepository.kt
@@ -0,0 +1,21 @@
+package com.mubin.domain.repo
+
+import com.mubin.network.model.Movie
+import com.mubin.network.util.NetworkResult
+
+interface MovieRepository {
+
+    suspend fun syncMoviesIfNeeded(): NetworkResult<Unit>
+
+    suspend fun getMoviesPaginated(limit: Int, offset: Int): List<Movie>
+
+    suspend fun getMoviesByGenrePaginated(genre: String, limit: Int, offset: Int): List<Movie>
+
+    suspend fun getMovieById(id: Int): Movie?
+
+    suspend fun updateWishlistStatus(id: Int, status: Boolean)
+
+    suspend fun getWishlist(): List<Movie>
+
+    suspend fun getAllGenres(): List<String>
+}
