a680f9cd516b29a68ae2ccdb873f7d104cbc5e0a
Mubin <mubin@kolpolok.com>
2025-08-07 02:00:23 +0600

    changes: core: network response models added

---
+++ core/network/src/main/java/com/mubin/network/model/Movie.kt
@@ -0,0 +1,34 @@
+package com.mubin.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("year")
+    val year: String,
+
+    @SerializedName("runtime")
+    val runtime: String,
+
+    @SerializedName("genres")
+    val genres: List<String>,
+
+    @SerializedName("director")
+    val director: String,
+
+    @SerializedName("actors")
+    val actors: String,
+
+    @SerializedName("plot")
+    val plot: String,
+
+    @SerializedName("posterUrl")
+    val posterUrl: String,
+
+    val isInWishlist: Boolean = false
+)
---
+++ core/network/src/main/java/com/mubin/network/model/MovieResponse.kt
@@ -0,0 +1,11 @@
+package com.mubin.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MoviesResponse(
+    @SerializedName("genres")
+    val genres: List<String> = emptyList(),
+
+    @SerializedName("movies")
+    val movies: List<Movie> = emptyList()
+)
