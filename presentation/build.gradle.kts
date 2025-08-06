d258ca9f9a3c69dd0a863c3d8c75d86c40af0850
Mubin <mubin@kolpolok.com>
2025-08-07 03:24:20 +0600

    changes: presentation module dependencies created

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 
 dependencies {
     implementation(project(":domain"))
+    implementation(project(":core:network"))
+    implementation(project(":core:database"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
