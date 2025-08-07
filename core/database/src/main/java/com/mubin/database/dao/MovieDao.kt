ffffe2b4b635dac3f2e25692ac5623bce53e02d0
Mubin <mubin@kolpolok.com>
2025-08-08 04:50:19 +0600

    changes: new queries added as per requirements

--- core/database/src/main/java/com/mubin/database/dao/MovieDao.kt
+++ core/database/src/main/java/com/mubin/database/dao/MovieDao.kt
@@ -15,11 +15,37 @@ interface MovieDao {
     @Query("SELECT COUNT(*) FROM movies")
     suspend fun getMovieCount(): Int
 
-    @Query("SELECT * FROM movies LIMIT :limit OFFSET :offset")
+    @Query("SELECT * FROM movies ORDER BY year DESC LIMIT :limit OFFSET :offset")
     suspend fun getMoviesPaginated(limit: Int, offset: Int): List<MovieEntity>
 
-    @Query("SELECT * FROM movies WHERE genres LIKE '%' || :genre || '%' LIMIT :limit OFFSET :offset")
+    @Query("""
+    SELECT * FROM movies 
+    WHERE genres LIKE '%' || :genre || '%' 
+    ORDER BY year DESC 
+    LIMIT :limit OFFSET :offset
+    """)
     suspend fun getMoviesByGenrePaginated(genre: String, limit: Int, offset: Int): List<MovieEntity>
+
+    @Query("""
+    SELECT * FROM movies
+    WHERE LOWER(title) LIKE '%' || LOWER(:query) || '%'
+    ORDER BY year DESC
+    LIMIT :limit OFFSET :offset
+    """)
+    suspend fun searchMoviesPaginated(query: String, limit: Int, offset: Int): List<MovieEntity>
+
+    @Query("""
+    SELECT * FROM movies 
+    WHERE genres LIKE '%' || :genre || '%' AND LOWER(title) LIKE '%' || LOWER(:query) || '%'
+    ORDER BY year DESC
+    LIMIT :limit OFFSET :offset
+    """)
+    suspend fun searchMoviesByGenrePaginated(
+        genre: String,
+        query: String,
+        limit: Int,
+        offset: Int
+    ): List<MovieEntity>
 
     @Query("SELECT * FROM movies WHERE id = :id")
     suspend fun getMovieById(id: Int): MovieEntity?
@@ -27,7 +53,7 @@ interface MovieDao {
     @Query("UPDATE movies SET isInWishlist = :status WHERE id = :id")
     suspend fun updateWishlistStatus(id: Int, status: Boolean)
 
-    @Query("SELECT * FROM movies WHERE isInWishlist = 1")
+    @Query("SELECT * FROM movies WHERE isInWishlist = 1 ORDER BY year DESC")
     suspend fun getWishlist(): List<MovieEntity>
 
 }
