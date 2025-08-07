819465c1e3cfacb0f134feb4e1b7b6063ed03bba
Mubin <maksudmubin@gmail.com>
2025-08-07 16:41:59 +0600

    changes: UseCaseModule.kt created to provide UseCase singletons

--- domain/src/main/java/com/mubin/domain/di/UseCaseModule.kt
+++ domain/src/main/java/com/mubin/domain/di/UseCaseModule.kt
@@ -1,10 +1,14 @@
 package com.mubin.domain.di
 
 import com.mubin.domain.repo.MovieRepository
-import com.mubin.domain.usecase.GetGenres
-import com.mubin.domain.usecase.GetPaginatedMovies
-import com.mubin.domain.usecase.GetPaginatedMoviesByGenre
-import com.mubin.domain.usecase.InitializeMovies
+import com.mubin.domain.usecase.GetAllGenresUseCase
+import com.mubin.domain.usecase.GetMovieByIdUseCase
+import com.mubin.domain.usecase.GetMoviesByGenrePaginatedUseCase
+import com.mubin.domain.usecase.GetMoviesPaginatedUseCase
+import com.mubin.domain.usecase.GetTotalMovieCountUseCase
+import com.mubin.domain.usecase.GetWishlistUseCase
+import com.mubin.domain.usecase.SyncMoviesIfNeededUseCase
+import com.mubin.domain.usecase.UpdateWishlistStatusUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,27 +20,34 @@ import javax.inject.Singleton
 object UseCaseModule {
 
     @Provides
-    @Singleton
-    fun provideInitializeMovies(
-        repo: MovieRepository
-    ): InitializeMovies = InitializeMovies(repo)
+    fun provideSyncMoviesIfNeededUseCase(repository: MovieRepository) =
+        SyncMoviesIfNeededUseCase(repository)
 
     @Provides
-    @Singleton
-    fun provideGetPaginatedMovies(
-        repo: MovieRepository
-    ): GetPaginatedMovies = GetPaginatedMovies(repo)
+    fun provideGetTotalMovieCountUseCase(repository: MovieRepository) =
+        GetTotalMovieCountUseCase(repository)
 
     @Provides
-    @Singleton
-    fun provideGetPaginatedMoviesByGenre(
-        repo: MovieRepository
-    ): GetPaginatedMoviesByGenre = GetPaginatedMoviesByGenre(repo)
+    fun provideGetMoviesPaginatedUseCase(repository: MovieRepository) =
+        GetMoviesPaginatedUseCase(repository)
 
     @Provides
-    @Singleton
-    fun provideGetGenres(
-        repo: MovieRepository
-    ): GetGenres = GetGenres(repo)
+    fun provideGetMoviesByGenrePaginatedUseCase(repository: MovieRepository) =
+        GetMoviesByGenrePaginatedUseCase(repository)
 
+    @Provides
+    fun provideGetMovieByIdUseCase(repository: MovieRepository) =
+        GetMovieByIdUseCase(repository)
+
+    @Provides
+    fun provideUpdateWishlistStatusUseCase(repository: MovieRepository) =
+        UpdateWishlistStatusUseCase(repository)
+
+    @Provides
+    fun provideGetWishlistUseCase(repository: MovieRepository) =
+        GetWishlistUseCase(repository)
+
+    @Provides
+    fun provideGetAllGenresUseCase(repository: MovieRepository) =
+        GetAllGenresUseCase(repository)
 }
