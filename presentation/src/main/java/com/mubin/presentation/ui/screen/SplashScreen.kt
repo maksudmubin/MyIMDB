4df332f1a4ddbbb5e8af7f54f215297faffd2edb
Mubin <mubin@kolpolok.com>
2025-08-08 18:43:58 +0600

    changes: app icon added in SplashScreen.kt

--- presentation/src/main/java/com/mubin/presentation/ui/screen/SplashScreen.kt
+++ presentation/src/main/java/com/mubin/presentation/ui/screen/SplashScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -25,10 +23,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mubin.presentation.R
 import com.mubin.presentation.ui.HomeViewModel
 import com.mubin.presentation.ui.util.NetworkHelper
 import kotlinx.coroutines.delay
@@ -72,14 +73,14 @@ fun SplashScreen(
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Icon(
-                imageVector = Icons.Default.Star,
+                painter = painterResource(R.drawable.movie_app_icon),
                 contentDescription = "App Logo",
-                tint = MaterialTheme.colorScheme.onBackground,
-                modifier = Modifier.size(64.dp)
+                tint = Color.Unspecified,
+                modifier = Modifier.size(150.dp)
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = "MyIMDB",
+                text = "My IMDB",
                 color = MaterialTheme.colorScheme.onBackground,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
