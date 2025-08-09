66b95eb419e1ffed0e4e92dbc57ea03a54223c96
Mubin <mubin@kolpolok.com>
2025-08-09 21:38:03 +0600

    changes: code documentation added in MainApplication.kt

--- app/src/main/java/com/mubin/myimdb/application/MainApplication.kt
+++ app/src/main/java/com/mubin/myimdb/application/MainApplication.kt
@@ -1,7 +1,23 @@
 package com.mubin.myimdb.application
 
 import android.app.Application
+import com.mubin.common.utils.logger.MyImdbLogger
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * The MainApp class serves as the entry point for the application.
+ * Annotated with @HiltAndroidApp, it sets up Dagger Hilt's dependency injection framework.
+ */
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApp : Application() {
+
+    /**
+     * Called when the application is starting, before any other application objects have been created.
+     * Override this method to perform one-time setup operations.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        // Log a message indicating that the application has been initialized
+        MyImdbLogger.d("MainApp", "Application has started")
+    }
+}
