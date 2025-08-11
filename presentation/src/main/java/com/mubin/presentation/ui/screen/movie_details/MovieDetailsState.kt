a3c0a893bee8b0595291e19f366cd7943596035b
Mubin <mubin@kolpolok.com>
2025-08-11 17:09:04 +0600

    changes: wishlistCount added in MovieDetailsState.kt

--- presentation/src/main/java/com/mubin/presentation/ui/screen/movie_details/MovieDetailsState.kt
+++ presentation/src/main/java/com/mubin/presentation/ui/screen/movie_details/MovieDetailsState.kt
@@ -5,6 +5,7 @@ import com.mubin.domain.model.Movie
 data class MovieDetailsState(
     val movie: Movie? = null,
     val isInWishlist: Boolean = false,
+    val wishlistCount: Int = 0,
     val isLoading: Boolean = false,
     val error: String? = null
 )
